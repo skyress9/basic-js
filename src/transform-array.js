4420b4fa19688d19c84a51b72fa0f734462fceb2
skyress9 <lion.super2@gmail.com>
2021-09-12 18:51:15 +0300

    feat: complete transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(array) {
+  let error = new Error("\'arr\' parameter must be an instance of the Array!")
+
+  if (!(array instanceof Array)) {
+    throw error;
+  }
+
+  let arr = array.slice();
+
+  if (arr[0] == '--double-prev' || arr[0] == '--discard-prev') {
+    arr.shift();
+  } else if (arr[arr.length - 1] == '--double-next' || arr[arr.length - 1] == '--discard-next') {
+    arr.pop();
+  }
+ 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+      arr[i] = '';
+      arr[i + 1] = ''
+    }
+    if (arr[i] == '--discard-prev') {
+      arr[i] = '';
+      arr[i - 1] = ''
+    }
+    if (arr[i] == '--double-next') {
+      arr[i] = arr[i + 1];
+    }
+    if (arr[i] == '--double-prev') {
+      arr[i] = arr[i - 1];
+    }
+  }
+
+  return arr.filter(e => {
+    if (e != '') {
+      return e;
+    }
+  });
 }
