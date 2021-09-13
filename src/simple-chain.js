2e122f493fe554bbf57553c10334176a65daa34a
skyress9 <lion.super2@gmail.com>
2021-09-13 18:07:08 +0300

    feat: complete simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${typeof value == 'undefined' ? '' : value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position < 1 || Math.trunc(position) !== position || position > this.arr.length) {
+      this.arr = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.arr.splice((position - 1), 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.join('~~');
+    this.arr = [];
+    return result
   }
 };
